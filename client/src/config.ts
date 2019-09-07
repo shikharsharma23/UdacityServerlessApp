802628ee4ee7955abda4f9e149dc06441a752a38
Shikhar Sharma <shikharcic23@gmail.com>
2019-09-07 16:57:25 +0530

    AWS + SLS framework

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'r1wcd8vguf';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-v26hgl6u.auth0.com',
+  clientId: 'WnCt2oLbomgRKJn7tn4naf00FBwVnmSj',
+  callbackUrl: 'http://localhost:3000/callback'
+};
